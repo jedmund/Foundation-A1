c99a71c8ced970bd4e90553765ed11058dc51947
Justin Edmund <justin@jedmund.com>
2011-02-21 17:04:24 -0500

    Installation should run flawlessly now.

--- install/index.php
+++ install/index.php
@@ -1,4 +1,4 @@
-<?php	
+<?php
 	if (file_exists('lib/init/config.php')) {
 		echo "Don't run this.";
 	} else if (empty($_POST)) {
@@ -10,15 +10,15 @@
 		$dbusername = stripslashes($_POST['db_username']);
 		$dbpassword = stripslashes($_POST['db_password']);
 		$dbname 		= stripslashes($_POST['db_name']);
-		
-		// Get the structure of the constants.php file and replace its 
+
+		// Get the structure of the constants.php file and replace its
 		// placeholder text with the values we cleaned.
-		$constants = file_get_contents('constants.txt');		
+		$constants = file_get_contents('constants.txt');
 		$constants = str_replace('dbserver', '"' . $dbserver . '"', $constants);
-		$constants = str_replace('dbuser', '"' . $dbusername . '"', $constants);	
+		$constants = str_replace('dbuser', '"' . $dbusername . '"', $constants);
 		$constants = str_replace('dbpass', '"' . $dbpassword . '"', $constants);
 		$constants = str_replace('dbname', '"' . $dbname . '"', $constants);
-		
+
 		// Open a file path to where the actual constants.php should be and
 		// write our values.
 		$fp = "../lib/init/constants.php";
@@ -27,25 +27,28 @@
 			chmod($fp, 0755);
 			fclose($file);
 		}
-		
-		// Explode the results of getcwd() to get the absolute path, then 
+
+		// Explode the results of getcwd() to get the absolute path, then
 		// replace with directory separators.
 		$path = explode("/", getcwd());
 		$key = array_shift($path);
-		$key = array_pop($path);		
+		$key = array_pop($path);
+		
 		$path = "DS.'" . implode("'.DS.'", $path) . "'";
+		$path = str_replace(array(".DS.", "DS."), DIRECTORY_SEPARATOR, $path);
+		$path = str_replace("'", "", $path);
 		
 		// Get the domain name and whatnot from the SERVER superglobal.
 		$domain = $_SERVER['HTTP_REFERER'];
 		$index = strpos($domain, "install");
-		$domain = substr($domain, 0, $index);		
-		
-		// Get the structure of the config.txt file and replace its 
+		$domain = substr($domain, 0, $index);
+
+		// Get the structure of the config.txt file and replace its
 		// placeholder text with the values we calculated.
 		$config = file_get_contents('config.txt');
 		$config = str_replace('siteroot', $path, $config);
 		$config = str_replace('sitedomain', $domain, $config);
-		
+
 		// Open a file path to where the actual config.php should be and
 		// write our values.
 		$fp = "../lib/init/config.php";
@@ -54,26 +57,25 @@
 			chmod($fp, 0755);
 			fclose($file);
 		}
-		
-		// Include the newly created config.php file, as well as the 
+
+		// Include the newly created config.php file, as well as the
 		// database installer file.
 		include($fp);
 		include('db.php');
-		
-		
+
+
 		// Get the structure of the root .htaccess file and keep it in
 		// memory.
 		$htaccess = file_get_contents('htaccess.txt');
-		
+
 		// Open a file to where the .htaccess file should be in root and
 		// write our values.
 		$fp = "../.htaccess";
 		if ($file = fopen($fp, "w")) {
 			fwrite($file, $htaccess);
-			chmod($fp, 0644);
 			fclose($file);
 		}
-		
+
 		// Execute every query, one-by-one.
 		for ($i = 0; $i < count($sql); $i++) {
 			if (!$db->query($sql[$i])) {
@@ -82,7 +84,7 @@
 				die();
 			}
 		}
-		
+
 		// Check to make sure that the user has supplied matching passwords.
 		// If they have, create a new user. Explicitly set the UID since
 		// Foundation is in forced single-user mode right now.
@@ -92,22 +94,24 @@
 			die();
 		} else {
 			$user = new User;
-			$user->id = 10;
 			$user->username = $db->escape_value($_POST['username']);
+			
 			$salt = hash("sha1", strtolower($user->username));
 			$password = crypt($_POST['password'], $salt);
 			$user->password = $password;
-			$user->save();
 			
+			$user->save();
+
 			// Also, create a user folder.
-			mkdir(PUBLIC_PATH.DS.'content'.DS.$user->username, 0777);
+			mkdir($path.'/content/'.$user->username, 0777);
 		}
-		
+
 		// Recursively remove the installer folder and all of its contents.
-		rrmdir($path."/install");
+		rrmdir($path.DS."install");
+		unlink($path.DS."router.php");
 
 		// Redirect the user to the login page.
-		header('/admin/login.php');
+		header('Location: /admin/login.php');
 	}
 
 ?>
